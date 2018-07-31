0341ac84d74e1c63bd39c7e66e67052920bd8b90
siddharthshashikar <siddharth.shashikar@st.niitnuiversity.in>
2018-07-31 19:35:28 +0530

    coverted 2 gray

--- readVideoWebcam.py
+++ readVideoWebcam.py
@@ -7,11 +7,14 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame',frame)
+
+    # converting gray scale
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame_gray',gray)
+    cv2.imshow('frame_color',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
-
 cap.release()
 cv2.destroyAllWindows()
