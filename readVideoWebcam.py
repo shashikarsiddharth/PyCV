deca45adc0721a193e1ecae1f43396371d3ec6ff
siddharthshashikar <siddharth.shashikar@st.niitnuiversity.in>
2018-07-31 19:42:54 +0530

    save output file

--- readVideoWebcam.py
+++ readVideoWebcam.py
@@ -4,17 +4,21 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
 
 while True:
     ret, frame = cap.read()
 
     # converting gray scale
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame_gray',gray)
+    # cv2.imshow('frame_gray',gray)
+    out.write(frame)
     cv2.imshow('frame_color',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
 cap.release()
+out.release()
 cv2.destroyAllWindows()
